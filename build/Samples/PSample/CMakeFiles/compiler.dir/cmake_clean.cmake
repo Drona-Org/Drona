04b4a8c6ff37b5338fe840c5fff093f273e663a0
tommasodreossi <tommasodreossi@gmail.com>
2017-01-22 12:30:59 -0800

    removed build

--- build/Samples/PSample/CMakeFiles/compiler.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/compiler"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang)
-  include(CMakeFiles/compiler.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/Samples/PSample/CMakeFiles/linker.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/linker"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang)
-  include(CMakeFiles/linker.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
