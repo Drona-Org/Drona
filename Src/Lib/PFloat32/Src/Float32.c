4fff88d1e3d3752e0ad13c211452bb39915afda5
Ankush Desai <ankushdesai@gmail.com>
2017-01-23 00:45:22 -0800

    updated the floating point implementation

--- Src/Lib/PFloat32/Src/Float32.c
+++ Src/Lib/PFloat32/Src/Float32.c
@@ -7,69 +7,8 @@ typedef union PRT_LIB_FLOAT32_DATA
 	float value;
 } PRT_LIB_FLOAT32_DATA;
 
-void PrtLibFloat32Init(PRT_LIB_FLOAT32_DATA *data)
-{
-	data->bits = 0;
-}
+// These are the required foreign core functions.
 
-void PrtLibFloat32GetFArgs0(PRT_MACHINEINST *context)
-{
-	PRT_MACHINEINST_PRIV *p_tmp_mach_priv = (PRT_MACHINEINST_PRIV *)context;
-	PRT_FUNSTACK_INFO p_tmp_frame;
-	PrtPopFrame(p_tmp_mach_priv, &p_tmp_frame);
-	PrtFreeLocals(p_tmp_mach_priv, &p_tmp_frame);
-}
-
-void PrtLibFloat32GetFArgs1(PRT_MACHINEINST *context, PRT_LIB_FLOAT32_DATA *arg1)
-{
-	PRT_MACHINEINST_PRIV *p_tmp_mach_priv = (PRT_MACHINEINST_PRIV *)context;
-	PRT_FUNSTACK_INFO p_tmp_frame;
-	PrtPopFrame(p_tmp_mach_priv, &p_tmp_frame);
-	arg1->bits = PrtGetForeignValue(p_tmp_frame.locals[0])->value;
-	PrtFreeLocals(p_tmp_mach_priv, &p_tmp_frame);
-}
-
-void PrtLibFloat32GetIArgs1(PRT_MACHINEINST *context, PRT_INT32 *arg1)
-{
-	PRT_MACHINEINST_PRIV *p_tmp_mach_priv = (PRT_MACHINEINST_PRIV *)context;
-	PRT_FUNSTACK_INFO p_tmp_frame;
-	PrtPopFrame(p_tmp_mach_priv, &p_tmp_frame);
-	*arg1 = PrtPrimGetInt(p_tmp_frame.locals[0]);
-	PrtFreeLocals(p_tmp_mach_priv, &p_tmp_frame);
-}
-
-void PrtLibFloat32GetFArgs2(PRT_MACHINEINST *context, PRT_LIB_FLOAT32_DATA *arg1, PRT_LIB_FLOAT32_DATA *arg2)
-{
-	PRT_MACHINEINST_PRIV *p_tmp_mach_priv = (PRT_MACHINEINST_PRIV *)context;
-	PRT_FUNSTACK_INFO p_tmp_frame;
-	PrtPopFrame(p_tmp_mach_priv, &p_tmp_frame);
-	arg1->bits = PrtGetForeignValue(p_tmp_frame.locals[0])->value;
-	arg2->bits = PrtGetForeignValue(p_tmp_frame.locals[1])->value;
-	PrtFreeLocals(p_tmp_mach_priv, &p_tmp_frame);
-}
-
-void PrtLibFloat32GetFArgs3(PRT_MACHINEINST *context, PRT_LIB_FLOAT32_DATA *arg1, PRT_LIB_FLOAT32_DATA *arg2, PRT_LIB_FLOAT32_DATA *arg3)
-{
-	PRT_MACHINEINST_PRIV *p_tmp_mach_priv = (PRT_MACHINEINST_PRIV *)context;
-	PRT_FUNSTACK_INFO p_tmp_frame;
-	PrtPopFrame(p_tmp_mach_priv, &p_tmp_frame);
-	arg1->bits = PrtGetForeignValue(p_tmp_frame.locals[0])->value;
-	arg2->bits = PrtGetForeignValue(p_tmp_frame.locals[1])->value;
-	arg3->bits = PrtGetForeignValue(p_tmp_frame.locals[2])->value;
-	PrtFreeLocals(p_tmp_mach_priv, &p_tmp_frame);
-}
-
-void PrtLibFloat32GetIArgs2(PRT_MACHINEINST *context, PRT_INT32 *arg1, PRT_INT32 *arg2)
-{
-	PRT_MACHINEINST_PRIV *p_tmp_mach_priv = (PRT_MACHINEINST_PRIV *)context;
-	PRT_FUNSTACK_INFO p_tmp_frame;
-	PrtPopFrame(p_tmp_mach_priv, &p_tmp_frame);
-	*arg1 = PrtPrimGetInt(p_tmp_frame.locals[0]);
-	*arg2 = PrtPrimGetInt(p_tmp_frame.locals[1]);
-	PrtFreeLocals(p_tmp_mach_priv, &p_tmp_frame);
-}
-
-//// Nothing to do, frgnVal is the float
 void PRT_FORGN_FREE_float32_IMPL(PRT_UINT64 frgnVal)
 {
 }
@@ -82,19 +21,11 @@ PRT_BOOLEAN PRT_FORGN_ISEQUAL_float32_IMPL(PRT_UINT64 frgnVal1, PRT_UINT64 frgnV
 	return arg1.value == arg2.value;
 }
 
-//// Currently not implemented
 PRT_STRING PRT_FORGN_TOSTRING_float32_IMPL(PRT_UINT64 frgnVal)
 {
 	PRT_STRING temp = PrtMalloc(50);
 	sprintf_s(temp, 50, "%f", ((PRT_LIB_FLOAT32_DATA*)&frgnVal)->value);
 	return temp;
-}
-
-float PRT_FORGN_TOFLOAT_float32_IMPL(PRT_VALUE* frgnVal)
-{
-    PRT_LIB_FLOAT32_DATA arg;
-    arg.bits = PrtGetForeignValue(frgnVal)->value;
-    return arg.value;
 }
 
 PRT_UINT32 PRT_FORGN_GETHASHCODE_float32_IMPL(PRT_UINT64 frgnVal)
@@ -129,7 +60,6 @@ PRT_UINT32 PRT_FORGN_GETHASHCODE_float32_IMPL(PRT_UINT64 frgnVal)
 PRT_UINT64 PRT_FORGN_MKDEF_float32_IMPL(void)
 {
 	PRT_LIB_FLOAT32_DATA def;
-	PrtLibFloat32Init(&def);
 	def.value = 0;
 	return def.bits;
 }
@@ -139,181 +69,153 @@ PRT_UINT64 PRT_FORGN_CLONE_float32_IMPL(PRT_UINT64 frgnVal)
 	return frgnVal;
 }
 
-PRT_VALUE *P_FUN_AssignFloat32_IMPL(PRT_MACHINEINST *context)
+
+//  Model functions
+
+PRT_VALUE *P_FUN_NaNFloat32_FOREIGN(PRT_MACHINEINST *context)
 {
-	PRT_LIB_FLOAT32_DATA arg1, arg2;
-	PrtLibFloat32GetFArgs2(context, &arg1, &arg2);
-	arg1.value = arg2.value;
-	return PrtMkForeignValue(arg1.bits, P_GEND_TYPE_float32->typeUnion.typeTag);
+	return PrtMkFloat32Value(NAN);
 }
 
-PRT_VALUE *P_FUN_NaNFloat32_IMPL(PRT_MACHINEINST *context)
+PRT_VALUE *P_FUN_AddFloat32_FOREIGN(PRT_MACHINEINST *context, PRT_VALUE *op1, PRT_VALUE *op2)
 {
-	PrtLibFloat32GetFArgs0(context);
-	PRT_LIB_FLOAT32_DATA arg;
-	arg.value = NAN;
-	return PrtMkForeignValue(arg.bits, P_GEND_TYPE_float32->typeUnion.typeTag);
+	float value1 = PrtGetFloat32(op1);
+	float value2 = PrtGetFloat32(op2);
+	return PrtMkFloat32Value(value1 + value2);
 }
 
-PRT_VALUE *P_FUN_AddFloat32_IMPL(PRT_MACHINEINST *context)
+PRT_VALUE *P_FUN_DivFloat32_FOREIGN(PRT_MACHINEINST *context, PRT_VALUE *op1, PRT_VALUE *op2)
 {
-	PRT_LIB_FLOAT32_DATA arg1, arg2;
-	PrtLibFloat32GetFArgs2(context, &arg1, &arg2);
-	arg1.value += arg2.value;	
-	return PrtMkForeignValue(arg1.bits, P_GEND_TYPE_float32->typeUnion.typeTag);
+	float value1 = PrtGetFloat32(op1);
+	float value2 = PrtGetFloat32(op2);
+	return PrtMkFloat32Value(value1 / value2);
 }
 
-PRT_VALUE *P_FUN_DivFloat32_IMPL(PRT_MACHINEINST *context)
+PRT_VALUE *P_FUN_IsNearFloat32_FOREIGN(PRT_MACHINEINST *context, PRT_VALUE *op1, PRT_VALUE *op2, PRT_VALUE *epsilon)
 {
-	PRT_LIB_FLOAT32_DATA arg1, arg2;
-	PrtLibFloat32GetFArgs2(context, &arg1, &arg2);
-	arg1.value /= arg2.value;
-	return PrtMkForeignValue(arg1.bits, P_GEND_TYPE_float32->typeUnion.typeTag);
-}
-
-PRT_VALUE *P_FUN_IsNearFloat32_IMPL(PRT_MACHINEINST *context)
-{
-	PRT_LIB_FLOAT32_DATA arg1, arg2, arg3;
-	PrtLibFloat32GetFArgs3(context, &arg1, &arg2, &arg3);
-	PRT_BOOLEAN result = (fabs(arg1.value - arg2.value) <= arg3.value) ? PRT_TRUE : PRT_FALSE;
+	float value1 = PrtGetFloat32(op1);
+	float value2 = PrtGetFloat32(op2);
+	float e = PrtGetFloat32(epsilon);
+	PRT_BOOLEAN result = (fabs(value1 - value2) <= e) ? PRT_TRUE : PRT_FALSE;
 	return PrtMkBoolValue(result);
 }
 
-
-PRT_VALUE *P_FUN_AbsFloat32_IMPL(PRT_MACHINEINST *context)
+PRT_VALUE *P_FUN_AbsFloat32_FOREIGN(PRT_MACHINEINST *context, PRT_VALUE *op1)
 {
-	PRT_LIB_FLOAT32_DATA arg1, arg2;
-	PrtLibFloat32GetFArgs2(context, &arg1, &arg2);
-	arg1.value = (float)fabs(arg2.value);
-	return PrtMkForeignValue(arg1.bits, P_GEND_TYPE_float32->typeUnion.typeTag);
+	float value = PrtGetFloat32(op1);
+	value = (float)fabs(value);
+	return PrtMkFloat32Value(value);
 }
 
-PRT_VALUE *P_FUN_IntToFloat32_IMPL(PRT_MACHINEINST *context)
+PRT_VALUE *P_FUN_IntToFloat32_FOREIGN(PRT_MACHINEINST *context, PRT_VALUE *val)
 {
-	PRT_INT32 i;
-	PRT_LIB_FLOAT32_DATA f;
-	PrtLibFloat32Init(&f);
-	PrtLibFloat32GetIArgs1(context, &i);
-	f.value = (float)i;
-	return PrtMkForeignValue(f.bits, P_GEND_TYPE_float32->typeUnion.typeTag);
+	PRT_INT32 i = PrtPrimGetInt(val);
+	return PrtMkFloat32Value((float)i);
 }
 
-PRT_VALUE *P_FUN_IsEqFloat32_IMPL(PRT_MACHINEINST *context)
+PRT_VALUE *P_FUN_IsEqFloat32_FOREIGN(PRT_MACHINEINST *context, PRT_VALUE *op1, PRT_VALUE *op2)
 {
-	PRT_LIB_FLOAT32_DATA arg1, arg2;
-	PrtLibFloat32GetFArgs2(context, &arg1, &arg2);
-	return PrtMkBoolValue(arg1.value == arg2.value ? PRT_TRUE : PRT_FALSE);
+	float value1 = PrtGetFloat32(op1);
+	float value2 = PrtGetFloat32(op2);
+	return PrtMkBoolValue(value1 == value2 ? PRT_TRUE : PRT_FALSE);
 }
 
-PRT_VALUE *P_FUN_IsFiniteFloat32_IMPL(PRT_MACHINEINST *context)
+PRT_VALUE *P_FUN_IsFiniteFloat32_FOREIGN(PRT_MACHINEINST *context, PRT_VALUE *op1)
 {
-	PRT_LIB_FLOAT32_DATA arg1;
-	PrtLibFloat32GetFArgs1(context, &arg1);
-	return PrtMkBoolValue(isfinite(arg1.value) ? PRT_TRUE : PRT_FALSE);
+	float value = PrtGetFloat32(op1);
+	return PrtMkBoolValue(isfinite(value) ? PRT_TRUE : PRT_FALSE);
 }
 
-PRT_VALUE *P_FUN_IsGeFloat32_IMPL(PRT_MACHINEINST *context)
+PRT_VALUE *P_FUN_IsGeFloat32_FOREIGN(PRT_MACHINEINST *context, PRT_VALUE *op1, PRT_VALUE *op2)
 {
-	PRT_LIB_FLOAT32_DATA arg1, arg2;
-	PrtLibFloat32GetFArgs2(context, &arg1, &arg2);
-	return PrtMkBoolValue(arg1.value >= arg2.value ? PRT_TRUE : PRT_FALSE);
+	float value1 = PrtGetFloat32(op1);
+	float value2 = PrtGetFloat32(op2);
+	return PrtMkBoolValue(value1 >= value2 ? PRT_TRUE : PRT_FALSE);
 }
 
-PRT_VALUE *P_FUN_IsGtFloat32_IMPL(PRT_MACHINEINST *context)
+PRT_VALUE *P_FUN_IsGtFloat32_FOREIGN(PRT_MACHINEINST *context, PRT_VALUE *op1, PRT_VALUE *op2)
 {
-	PRT_LIB_FLOAT32_DATA arg1, arg2;
-	PrtLibFloat32GetFArgs2(context, &arg1, &arg2);
-	return PrtMkBoolValue(arg1.value > arg2.value ? PRT_TRUE : PRT_FALSE);
+	float value1 = PrtGetFloat32(op1);
+	float value2 = PrtGetFloat32(op2);
+	return PrtMkBoolValue(value1 > value2 ? PRT_TRUE : PRT_FALSE);
 }
 
-PRT_VALUE *P_FUN_IsInftyFloat32_IMPL(PRT_MACHINEINST *context)
+PRT_VALUE *P_FUN_IsInfinityFloat32_FOREIGN(PRT_MACHINEINST *context, PRT_VALUE *op1)
 {
-	PRT_LIB_FLOAT32_DATA arg1;
-	PrtLibFloat32GetFArgs1(context, &arg1);
-	return PrtMkBoolValue(isinf(arg1.value) ? PRT_TRUE : PRT_FALSE);
+	float value = PrtGetFloat32(op1);
+	return PrtMkBoolValue(isinf(value) ? PRT_TRUE : PRT_FALSE);
 }
 
-PRT_VALUE *P_FUN_IsLeFloat32_IMPL(PRT_MACHINEINST *context)
+PRT_VALUE *P_FUN_IsLeFloat32_FOREIGN(PRT_MACHINEINST *context, PRT_VALUE *op1, PRT_VALUE *op2)
 {
-	PRT_LIB_FLOAT32_DATA arg1, arg2;
-	PrtLibFloat32GetFArgs2(context, &arg1, &arg2);
-	return PrtMkBoolValue(arg1.value <= arg2.value ? PRT_TRUE : PRT_FALSE);
+	float value1 = PrtGetFloat32(op1);
+	float value2 = PrtGetFloat32(op2);
+	return PrtMkBoolValue(value1 <= value2 ? PRT_TRUE : PRT_FALSE);
 }
 
-PRT_VALUE *P_FUN_IsLtFloat32_IMPL(PRT_MACHINEINST *context)
+PRT_VALUE *P_FUN_IsLtFloat32_FOREIGN(PRT_MACHINEINST *context, PRT_VALUE *op1, PRT_VALUE *op2)
 {
-	PRT_LIB_FLOAT32_DATA arg1, arg2;
-	PrtLibFloat32GetFArgs2(context, &arg1, &arg2);
-	return PrtMkBoolValue(arg1.value < arg2.value ? PRT_TRUE : PRT_FALSE);
+	float value1 = PrtGetFloat32(op1);
+	float value2 = PrtGetFloat32(op2);
+	return PrtMkBoolValue(value1 < value2 ? PRT_TRUE : PRT_FALSE);
 }
 
-PRT_VALUE *P_FUN_IsNEqFloat32_IMPL(PRT_MACHINEINST *context)
+PRT_VALUE *P_FUN_IsNEqFloat32_FOREIGN(PRT_MACHINEINST *context, PRT_VALUE *op1, PRT_VALUE *op2)
 {
-	PRT_LIB_FLOAT32_DATA arg1, arg2;
-	PrtLibFloat32GetFArgs2(context, &arg1, &arg2);
-	return PrtMkBoolValue(arg1.value != arg2.value ? PRT_TRUE : PRT_FALSE);
+	float value1 = PrtGetFloat32(op1);
+	float value2 = PrtGetFloat32(op2);
+	return PrtMkBoolValue(value1 != value2 ? PRT_TRUE : PRT_FALSE);
 }
 
-PRT_VALUE *P_FUN_IsNaNFloat32_IMPL(PRT_MACHINEINST *context)
+PRT_VALUE *P_FUN_IsNaNFloat32_FOREIGN(PRT_MACHINEINST *context, PRT_VALUE *op1)
 {
-	PRT_LIB_FLOAT32_DATA arg1;
-	PrtLibFloat32GetFArgs1(context, &arg1);
-	return PrtMkBoolValue(isnan(arg1.value) ? PRT_TRUE : PRT_FALSE);
+	float value = PrtGetFloat32(op1);
+	return PrtMkBoolValue(isnan(value) ? PRT_TRUE : PRT_FALSE);
 }
 
-PRT_VALUE *P_FUN_IsNegInftyFloat32_IMPL(PRT_MACHINEINST *context)
+PRT_VALUE *P_FUN_IsNegInfinityFloat32_FOREIGN(PRT_MACHINEINST *context, PRT_VALUE *op1)
 {
-	PRT_LIB_FLOAT32_DATA arg1;
-	PrtLibFloat32GetFArgs1(context, &arg1);
-	return PrtMkBoolValue(isinf(arg1.value) && arg1.value < 0 ? PRT_TRUE : PRT_FALSE);
+	float value = PrtGetFloat32(op1);
+	return PrtMkBoolValue(isinf(value) && value < 0 ? PRT_TRUE : PRT_FALSE);
 }
 
-PRT_VALUE *P_FUN_IsPosInftyFloat32_IMPL(PRT_MACHINEINST *context)
+PRT_VALUE *P_FUN_IsPosInfinityFloat32_FOREIGN(PRT_MACHINEINST *context, PRT_VALUE *op1)
 {
-	PRT_LIB_FLOAT32_DATA arg1;
-	PrtLibFloat32GetFArgs1(context, &arg1);
-	return PrtMkBoolValue(isinf(arg1.value) && arg1.value > 0 ? PRT_TRUE : PRT_FALSE);
+	float value = PrtGetFloat32(op1);
+	return PrtMkBoolValue(isinf(value) && value > 0 ? PRT_TRUE : PRT_FALSE);
 }
 
-PRT_VALUE *P_FUN_MulFloat32_IMPL(PRT_MACHINEINST *context)
+PRT_VALUE *P_FUN_MulFloat32_FOREIGN(PRT_MACHINEINST *context, PRT_VALUE *op1, PRT_VALUE *op2)
 {
-	PRT_LIB_FLOAT32_DATA arg1, arg2;
-	PrtLibFloat32GetFArgs2(context, &arg1, &arg2);
-	arg1.value *= arg2.value;
-	return PrtMkForeignValue(arg1.bits, P_GEND_TYPE_float32->typeUnion.typeTag);
+	float value1 = PrtGetFloat32(op1);
+	float value2 = PrtGetFloat32(op2);
+	return PrtMkFloat32Value(value1 * value2);
 }
 
-PRT_VALUE *P_FUN_NegFloat32_IMPL(PRT_MACHINEINST *context)
+PRT_VALUE *P_FUN_NegFloat32_FOREIGN(PRT_MACHINEINST *context, PRT_VALUE *op1)
 {
-	PRT_LIB_FLOAT32_DATA arg1;
-	PrtLibFloat32GetFArgs1(context, &arg1);
-	arg1.value = -arg1.value;
-	return PrtMkForeignValue(arg1.bits, P_GEND_TYPE_float32->typeUnion.typeTag);
+	float value = PrtGetFloat32(op1);
+	return PrtMkFloat32Value(-value);
 }
 
-PRT_VALUE *P_FUN_ScientificToFloat32_IMPL(PRT_MACHINEINST *context)
+PRT_VALUE *P_FUN_ScientificToFloat32_FOREIGN(PRT_MACHINEINST *context, PRT_VALUE *mantissaValue, PRT_VALUE *expValue)
 {
-	PRT_INT32 arg1, arg2;
-	PRT_LIB_FLOAT32_DATA f;
-	PrtLibFloat32Init(&f);
-	PrtLibFloat32GetIArgs2(context, &arg1, &arg2);
-	f.value = (float)arg1;
-	f.value *= (float)pow(10.0, (double)arg2);
-	return PrtMkForeignValue(f.bits, P_GEND_TYPE_float32->typeUnion.typeTag);
+	float mantissa = PrtGetFloat32(mantissaValue);
+	float exponent = PrtGetFloat32(expValue);
+	double result = (double)mantissa * pow(10.0, (double)exponent);
+	return PrtMkFloat32Value((float)result);
 }
 
-PRT_VALUE *P_FUN_SubFloat32_IMPL(PRT_MACHINEINST *context)
+PRT_VALUE *P_FUN_SubFloat32_FOREIGN(PRT_MACHINEINST *context, PRT_VALUE *op1, PRT_VALUE *op2)
 {
-	PRT_LIB_FLOAT32_DATA arg1, arg2;
-	PrtLibFloat32GetFArgs2(context, &arg1, &arg2);
-	arg1.value -= arg2.value;
-	return PrtMkForeignValue(arg1.bits, P_GEND_TYPE_float32->typeUnion.typeTag);
+	float value1 = PrtGetFloat32(op1);
+	float value2 = PrtGetFloat32(op2);
+	return PrtMkFloat32Value(value1 - value2);
 }
 
 PRT_VALUE* PrtMkFloat32Value(float value)
 {
 	PRT_LIB_FLOAT32_DATA f;
-	PrtLibFloat32Init(&f);
 	f.value = value;
 	return PrtMkForeignValue(f.bits, P_GEND_TYPE_float32->typeUnion.typeTag);
 }
