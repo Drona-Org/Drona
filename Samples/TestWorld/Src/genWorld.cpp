cd0a35ea9ee9e179f386bc3d5e925e0c5e688a9b
tommasodreossi <tommasodreossi@gmail.com>
2017-02-17 16:31:59 -0800

    new world generators

---
+++ Samples/TestWorld/Src/genWorld.cpp
@@ -0,0 +1,8 @@
+#include "World.h"
+
+
+int main()
+{
+
+}
+
---
+++ Src/Lib/World/Includes/World.h
@@ -0,0 +1,15 @@
+#ifndef WORLD
+#define WORLD
+
+using namespace std;
+
+class World
+{
+private:
+
+public:
+
+    World();
+
+};
+#endif // !World
