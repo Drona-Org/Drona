2f2856dbbaaba8658421b690a8e3f822230ca750
tommasodreossi <tommasodreossi@gmail.com>
2017-03-06 11:54:21 -0800

    start/stop matlab engine

--- Src/Lib/Breach/Includes/BreachAPI.h
+++ Src/Lib/Breach/Includes/BreachAPI.h
@@ -1,14 +1,25 @@
 #ifndef BREACHAPI
 #define BREACHAPI
 
+#include "engine.h"
+#include <iostream>
+
 using namespace std;
 
 
 class BreachAPI
 {
 private:
+    Engine *matEng;    // Matlab engine
+    bool engOn;
+    bool initBreach;
 
 public:
     BreachAPI();
+
+    bool StartMatlabEngine();
+    bool StopMatlabEngine();
+
+    bool InitBreach(char* pathToBreach);
 };
 #endif // !BreachAPI
--- Src/Lib/Breach/Src/BreachAPI.cpp
+++ Src/Lib/Breach/Src/BreachAPI.cpp
@@ -2,4 +2,51 @@
 
 BreachAPI::BreachAPI(){
 
+    this->engOn = false;
+    this->initBreach = false;
+
 }
+
+// Start Matlab engine
+bool BreachAPI::StartMatlabEngine(){
+
+    if(this->engOn){
+        cout<<"BreachAPI::StartMatlabEngine Matlab engine already on"<<endl;
+        return false;
+    }
+
+    cout<<"BreachAPI::StartMatlabEngine Starting Matlab engine..."<<endl;
+    if (!(this->matEng = engOpen(""))) {
+        cout<<"BreachAPI::StartMatlabEngine Can't start MATLAB engine"<<endl;
+        return false;
+    }else{
+        cout<<"BreachAPI::StartMatlabEngine MATLAB engine started"<<endl;
+        this->engOn = true;
+        return true;
+    }
+}
+
+// Stop Matlab engine
+bool BreachAPI::StopMatlabEngine(){
+
+    if(!this->engOn){
+        cout<<"BreachAPI::StopMatlabEngine Matlab engine already off"<<endl;
+        return false;
+    }
+
+    engClose(this->matEng);
+    this->engOn = false;
+    cout<<"BreachAPI::StopMatlabEngine MATLAB engine stopped"<<endl;
+}
+
+// Stop Matlab engine
+bool BreachAPI::InitBreach(char* pathToBreach){
+
+    if(this->initBreach){
+        cout<<"BreachAPI::InitBreach Breach already initialized"<<endl;
+        return false;
+    }
+
+    return true;
+}
+
