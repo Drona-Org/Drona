a2d1b00c84d138406f13a49c876f80826ad80d71
Ankush Desai <ankushdesai@gmail.com>
2017-08-04 15:18:48 -0700

    update

---
+++ Src/Lib/Loggers/ConsoleLogger/Includes/Logger.h
@@ -0,0 +1,32 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+
+#include<pthread.h>
+#include<stdio.h>
+#include<string>
+using namespace std;
+
+
+
+#define LOG(msg) GlobalLogger->LogMessage(msg)
+#define ERROR(msg) GlobalLogger->LogError(msg)
+#define DEBUG_LOG(msg) GlobalLogger->DebugLog(msg)
+
+class Logger
+{
+
+    pthread_mutex_t lock;
+public:
+    Logger()
+    {
+        pthread_mutex_init(&lock, NULL);
+    }
+    //print error message
+    void LogError(const char* msg);
+    void LogMessage(const char* msg);
+    void DebugLog(const char* msg);
+
+};
+extern Logger* GlobalLogger;
+
+#endif
