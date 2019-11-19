fa6b1d2718d1b8c700b2c2d5ab48f77ae69c770d
Sumukh Shivakumar <sumukhshiv@berkeley.edu>
2019-11-18 16:43:05 -0800

    cmake uses internal dependencies only

--- Src/Lib/ExternalMotionPlanners/OMPLOptimalPlanner/Includes/PlanGenerator.h
+++ Src/Lib/ExternalMotionPlanners/OMPLOptimalPlanner/Includes/PlanGenerator.h
@@ -150,6 +150,8 @@ public:
 
     OMPLPLanner(const char* workspacefile, optimalPlanner _plannerType, planningObjective _objectiveType)
     {
+	    printf("SHIVA!!!!\n");
+
         WSInfo = ParseWorkspaceConfig(workspacefile);
         plannerType = _plannerType;
         objectiveType = _objectiveType;
