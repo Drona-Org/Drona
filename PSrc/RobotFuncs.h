fd6e2861ae8ab7a161c8a3d4ad50c523a56cba0c
Sumukh Shivakumar <sumukhshiv@berkeley.edu>
2020-01-28 11:17:23 -0800

    removing unnecessary files

--- PSrc/RobotFuncs.h
+++
@@ -1,5 +0,0 @@
-#include "MailDeliveryTaskPlanner.h"
-
-extern "C" PRT_VALUE* P_omplMotionPlanExternal_IMPL(PRT_MACHINEINST* context, PRT_VALUE*** argRefs);
-extern "C" PRT_VALUE* P_ROSGoTo_IMPL(PRT_MACHINEINST* context, PRT_VALUE*** argRefs);
-extern "C" PRT_VALUE* P_Sleep_IMPL(PRT_MACHINEINST* context, PRT_VALUE*** argRefs);
